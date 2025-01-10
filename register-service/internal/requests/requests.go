2b2bc17b170f91ef269668049ecb556d966948ac
k.korobkina <k.korobkina@facct.ru>
2025-01-10 14:44:26 +0300

    adding validation and logs

--- register-service/internal/requests/requests.go
+++ register-service/internal/requests/requests.go
@@ -1,12 +1,12 @@
 package requests //запросы
 
 type Client struct {
-	Email    string `json:"email"`
-	FullName string `json:"full_name"`
-	Password string `json:"password"`
+	Email    string `json:"email" validate:"required,email"`
+	FullName string `json:"full_name" validate:"required,min=1"`
+	Password string `json:"password" validate:"required,min=1"`
 }
 
 type Login struct {
-	Email    string `json:"email"`
-	Password string `json:"password"`
+	Email    string `json:"email" validate:"required,email"`
+	Password string `json:"password" validate:"required,min=1"`
 }
